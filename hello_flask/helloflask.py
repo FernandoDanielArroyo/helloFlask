f7b54adacc208a4159ff180def2e4921bcf1a1e6
fdac <fernandodanielarroyo90@gmail.com>
2022-08-24 13:39:50 +0200

    j'ai ajouté les trois pages web mais la récupération d'info n'est pas tout à fait prête

--- hello_flask/helloflask.py
+++ hello_flask/helloflask.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-
+@app.route("/", methods = ['GET','POST'])
 def home():
-	return "<p>Bienvenue chez moi</p>"
-app.run()
+	if request.method == 'GET':
+		return render_template('index.html')
+	elif request.method == 'POST':
+		form_data = request.form
+		return render_template('about.html', titre = 'blabla')
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+@app.route("/form")
+def form ():
+	return render_template('form.html')
+
+app.run(debug=True)
 
 # import de l’objet Flask
 # instantiation application
